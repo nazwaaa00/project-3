bcaa5ae2aa0df20904a17fadcccdbf4418eb040d
nazwaaa00 <nazwameisya253@gmail.com>
2025-09-08 09:21:32 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cta = document.querySelector('.cta');
+  if (cta) {
+    cta.addEventListener('click', () => {
+      alert('Selamat belajar Memasakkk!!!!');
+    });
+  }
+});
